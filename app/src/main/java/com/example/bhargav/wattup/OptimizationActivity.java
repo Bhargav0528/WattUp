0ba452a37a42e4ae5eb07edd38d28dbdead94d29
Bhargav <bhargavauden1998@gmail.com>
2017-08-22 22:25:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bhargav/wattup/OptimizationActivity.java
@@ -0,0 +1,13 @@
+package com.example.bhargav.wattup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OptimizationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_optimization);
+    }
+}
